5a48e8efd8569931425aac33889886972a1dc8aa
Andres Marin <a307737@uach.mx>
2020-11-12 11:14:49 -0700

    Act20_21_22

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import Parrafo from './components/Parrafo'
 import Contador from './components/Contador'
 import User from './components/User'
+import Foco from './components/Foco'
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
-     <Parrafo></Parrafo>
-     <hr></hr>
-     <Contador></Contador>
-     <User></User>
+    <h1>Focos</h1>
+    <Foco lugar = "sala"></Foco>
+    <Foco lugar = "cocina"></Foco>
+    <Foco lugar = "recamara principal"></Foco>
+    <Foco lugar = "recamara de los niños"></Foco>
+    <Foco lugar = "comedor"></Foco>
+    <Foco lugar = "estancia"></Foco>
+    <hr></hr>
+    <h1>Párrafo</h1>
+    <Parrafo></Parrafo>
+    <hr></hr>
+    <h1>Contador</h1>
+    <Contador></Contador>
+    <hr></hr>
+    <h1>Consumir api usuarios</h1>
+    <User></User>
+     
     </div>
   );
 }
